b0fc2e1d6eff0b28d630dd302952b4280410dca5
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2025-12-10 23:27:49 +0800

    style(nav): init styling for breadcrumb

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -243,8 +243,8 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
             </div>
             
             {breadcrumbs.length > 0 && (
-                <div className="flex w-full border-b border-sidebar-border/70">
-                    <div className="mx-auto flex h-12 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
+                <div className="flex w-full bg-breadcrumb border-b border-sidebar-border/70">
+                    <div className="mx-auto flex h-12 w-full items-center justify-start px-4 bg-breadcrumb text-neutral-500 md:max-w-[1440px]">
                         <Breadcrumbs breadcrumbs={breadcrumbs} />
                     </div>
                 </div>
