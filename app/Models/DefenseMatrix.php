e1e511e6d2d0ca3937ad0b3c8d010d5b6cdc1276
Christian-Villanueva <christianaprilkimvillanueva@gmail.com>
2025-12-13 03:11:23 +0800

    feat(models): implement simple core thesis workflow models

--- app/Models/DefenseMatrix.php
+++ app/Models/DefenseMatrix.php
@@ -4,33 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Proposal extends Model
+class DefenseMatrix extends Model
 {
-    use HasFactory, SoftDeletes;    
-    
-    protected $table = 'tbl_proposals';
+    use HasFactory;
+
+    protected $table = 'tbl_defense_matrices';
 
     protected $fillable = [
-        'group_id',
-        'proposal_title',
-        'proposal_filepath',
-        'is_pursued',
+        'thesis_id',          
+        'course',           
+        'defense_schedule', 
+        'defense_room',
     ];
 
     protected $casts = [
-        'is_pursued' => 'boolean',
+        'defense_schedule' => 'datetime',
+        'defense_room'     => 'integer',
     ];
-
 
     /*
     ==================================================================================
     RELATIONSHIPS
     ==================================================================================
     */
-    public function group()
+
+    public function thesis()
     {
-        return $this->belongsTo(ThesisGroup::class, 'group_id');
+        return $this->belongsTo(Theses::class, 'thesis_id');
     }
 }
