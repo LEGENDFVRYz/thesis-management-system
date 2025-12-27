f828a0eb0287d1c8ed13c5c136f3bea6e18bec3b
sean <christianpespanol@iskolarngbayan.pup.edu.ph>
2025-12-27 21:33:43 +0800

    Icons and Badges

---
+++ resources/js/components/icons-registry.tsx
@@ -0,0 +1,245 @@
+﻿import ArchiveClickedIcon from "./Icons/ic_archive-Clicked.svg";
+import ArchiveDefaultIcon from "./Icons/ic_archive-Default.svg";
+import ArchiveHoverIcon from "./Icons/ic_archive-Hover.svg";
+import BackClickedIcon from "./Icons/ic_back-Clicked.svg";
+import BackDefaultIcon from "./Icons/ic_back-Default.svg";
+import BackHoverIcon from "./Icons/ic_back-Hover.svg";
+import CalendarClickedIcon from "./Icons/ic_calendar-Clicked.svg";
+import CalendarDefaultIcon from "./Icons/ic_calendar-Default.svg";
+import CalendarHoverIcon from "./Icons/ic_calendar-Hover.svg";
+import CheckClickedIcon from "./Icons/ic_check-Clicked.svg";
+import CheckDefaultIcon from "./Icons/ic_check-Default.svg";
+import CheckHoverIcon from "./Icons/ic_check-Hover.svg";
+import CheckCircleClickedIcon from "./Icons/ic_checkCircle-Clicked.svg";
+import CheckCircleDefaultIcon from "./Icons/ic_checkCircle-Default.svg";
+import CheckCircleHoverIcon from "./Icons/ic_checkCircle-Hover.svg";
+import ClockClickedIcon from "./Icons/ic_clock-Clicked.svg";
+import ClockDefaultIcon from "./Icons/ic_clock-Default.svg";
+import ClockHoverIcon from "./Icons/ic_clock-Hover.svg";
+import CloseClickedIcon from "./Icons/ic_close-Clicked.svg";
+import CloseDefaultIcon from "./Icons/ic_close-Default.svg";
+import CloseHoverIcon from "./Icons/ic_close-Hover.svg";
+import CommentClickedIcon from "./Icons/ic_comment-Clicked.svg";
+import CommentDefaultIcon from "./Icons/ic_comment-Default.svg";
+import CommentHoverIcon from "./Icons/ic_comment-Hover.svg";
+import DashboardClickedIcon from "./Icons/ic_dashboard-Clicked.svg";
+import DashboardDefaultIcon from "./Icons/ic_dashboard-Default.svg";
+import DashboardHoverIcon from "./Icons/ic_dashboard-Hover.svg";
+import DeleteClickedIcon from "./Icons/ic_delete-Clicked.svg";
+import DeleteDefaultIcon from "./Icons/ic_delete-Default.svg";
+import DeleteHoverIcon from "./Icons/ic_delete-Hover.svg";
+import DeletetableDefaultIcon from "./Icons/ic_deletetable-Default.svg";
+import DeletetableVariant2Icon from "./Icons/ic_deletetable-Variant2.svg";
+import DocuClickedIcon from "./Icons/ic_docu-Clicked.svg";
+import DocuDefaultIcon from "./Icons/ic_docu-Default.svg";
+import DocuHoverIcon from "./Icons/ic_docu-Hover.svg";
+import DownloadClickedIcon from "./Icons/ic_download-Clicked.svg";
+import DownloadDefaultIcon from "./Icons/ic_download-Default.svg";
+import DownloadHoverIcon from "./Icons/ic_download-Hover.svg";
+import EditClickedIcon from "./Icons/ic_edit-Clicked.svg";
+import EditDefaultIcon from "./Icons/ic_edit-Default.svg";
+import EditHoverIcon from "./Icons/ic_edit-Hover.svg";
+import EdittableDefaultIcon from "./Icons/ic_edittable-Default.svg";
+import EdittableVariant2Icon from "./Icons/ic_edittable-Variant2.svg";
+import EyecloseClickedIcon from "./Icons/ic_eyeclose-Clicked.svg";
+import EyecloseDefaultIcon from "./Icons/ic_eyeclose-Default.svg";
+import EyecloseHoverIcon from "./Icons/ic_eyeclose-Hover.svg";
+import EyeopenClickedIcon from "./Icons/ic_eyeopen-Clicked.svg";
+import EyeopenDefaultIcon from "./Icons/ic_eyeopen-Default.svg";
+import EyeopenHoverIcon from "./Icons/ic_eyeopen-Hover.svg";
+import FaqClickedIcon from "./Icons/ic_faq-Clicked.svg";
+import FaqDefaultIcon from "./Icons/ic_faq-Default.svg";
+import FaqHoverIcon from "./Icons/ic_faq-Hover.svg";
+import FeedbackClickedIcon from "./Icons/ic_feedback-Clicked.svg";
+import FeedbackDefaultIcon from "./Icons/ic_feedback-Default.svg";
+import FeedbackHoverIcon from "./Icons/ic_feedback-Hover.svg";
+import FilesClickedIcon from "./Icons/ic_files-Clicked.svg";
+import FilesDefaultIcon from "./Icons/ic_files-Default.svg";
+import FilesHoverIcon from "./Icons/ic_files-Hover.svg";
+import GotoClickedIcon from "./Icons/ic_goto-Clicked.svg";
+import GotoDefaultIcon from "./Icons/ic_goto-Default.svg";
+import GotoHoverIcon from "./Icons/ic_goto-Hover.svg";
+import HomeClickedIcon from "./Icons/ic_home-Clicked.svg";
+import HomeDefaultIcon from "./Icons/ic_home-Default.svg";
+import HomeHoverIcon from "./Icons/ic_home-Hover.svg";
+import LogoutClickedIcon from "./Icons/ic_logout-Clicked.svg";
+import LogoutDefaultIcon from "./Icons/ic_logout-Default.svg";
+import LogoutHoverIcon from "./Icons/ic_logout-Hover.svg";
+import MenuClickedIcon from "./Icons/ic_menu-Clicked.svg";
+import MenuDefaultIcon from "./Icons/ic_menu-Default.svg";
+import MenuHoverIcon from "./Icons/ic_menu-Hover.svg";
+import MessageClickedIcon from "./Icons/ic_message-Clicked.svg";
+import MessageDefaultIcon from "./Icons/ic_message-Default.svg";
+import MessageHoverIcon from "./Icons/ic_message-Hover.svg";
+import MoreClickedIcon from "./Icons/ic_more-Clicked.svg";
+import MoreDefaultIcon from "./Icons/ic_more-Default.svg";
+import MoreHoverIcon from "./Icons/ic_more-Hover.svg";
+import NficonsManagementIcon from "./Icons/ic_nficons-management.svg";
+import NficonsNegativeAlertIcon from "./Icons/ic_nficons-negative_alert.svg";
+import NficonsPanelsIcon from "./Icons/ic_nficons-panels.svg";
+import NficonsPositiveAlertIcon from "./Icons/ic_nficons-positive_alert.svg";
+import NficonsReminderIcon from "./Icons/ic_nficons-reminder.svg";
+import NotificationClickedIcon from "./Icons/ic_notification-Clicked-ic_notification2-notification.svg";
+import NotificationDefaultIcon from "./Icons/ic_notification-Default-ic_notification2-notification.svg";
+import NotificationHoverIcon from "./Icons/ic_notification-Hover-ic_notification2-notification.svg";
+import NotifyClickedIcon from "./Icons/ic_notify-Clicked.svg";
+import NotifyDefaultIcon from "./Icons/ic_notify-Default.svg";
+import NotifyHoverIcon from "./Icons/ic_notify-Hover.svg";
+import PeopleBoldIcon from "./Icons/ic_people-bold.svg";
+import PeopleLinearIcon from "./Icons/ic_people-linear.svg";
+import PeopleTwotoneIcon from "./Icons/ic_people-twotone.svg";
+import ProfileClickedIcon from "./Icons/ic_profile-Clicked.svg";
+import ProfileDefaultIcon from "./Icons/ic_profile-Default.svg";
+import ProfileHoverIcon from "./Icons/ic_profile-Hover.svg";
+import ProponentsClickedIcon from "./Icons/ic_proponents-Clicked.svg";
+import ProponentsDefaultIcon from "./Icons/ic_proponents-Default.svg";
+import ProponentsHoverIcon from "./Icons/ic_proponents-Hover.svg";
+import RepoClickedIcon from "./Icons/ic_repo-Clicked.svg";
+import RepoDefaultIcon from "./Icons/ic_repo-Default.svg";
+import RepoHoverIcon from "./Icons/ic_repo-Hover.svg";
+import SearchClicked1Icon from "./Icons/ic_search-Clicked-1.svg";
+import SearchClickedIcon from "./Icons/ic_search-Clicked.svg";
+import SearchDefaultIcon from "./Icons/ic_search-Default.svg";
+import SettingsClickedIcon from "./Icons/ic_settings-Clicked.svg";
+import SettingsDefaultIcon from "./Icons/ic_settings-Default.svg";
+import SettingsHoverIcon from "./Icons/ic_settings-Hover.svg";
+import ShareClickedIcon from "./Icons/ic_share-Clicked.svg";
+import ShareDefaultIcon from "./Icons/ic_share-Default.svg";
+import ShareHoverIcon from "./Icons/ic_share-Hover.svg";
+import SignClickedIcon from "./Icons/ic_sign-Clicked.svg";
+import SignDefaultIcon from "./Icons/ic_sign-Default.svg";
+import SignHoverIcon from "./Icons/ic_sign-Hover.svg";
+import SortClickedIcon from "./Icons/ic_sort-Clicked.svg";
+import SortDefaultIcon from "./Icons/ic_sort-Default.svg";
+import SortHoverIcon from "./Icons/ic_sort-Hover.svg";
+import TableClickedIcon from "./Icons/ic_table-Clicked.svg";
+import TableDefaultIcon from "./Icons/ic_table-Default.svg";
+import TableHoverIcon from "./Icons/ic_table-Hover.svg";
+import UploadClickedIcon from "./Icons/ic_upload-Clicked.svg";
+import UploadDefaultIcon from "./Icons/ic_upload-Default.svg";
+import UploadHoverIcon from "./Icons/ic_upload-Hover.svg";
+
+export const iconRegistry = {
+  archiveClicked: ArchiveClickedIcon,
+  archiveDefault: ArchiveDefaultIcon,
+  archiveHover: ArchiveHoverIcon,
+  backClicked: BackClickedIcon,
+  backDefault: BackDefaultIcon,
+  backHover: BackHoverIcon,
+  calendarClicked: CalendarClickedIcon,
+  calendarDefault: CalendarDefaultIcon,
+  calendarHover: CalendarHoverIcon,
+  checkClicked: CheckClickedIcon,
+  checkDefault: CheckDefaultIcon,
+  checkHover: CheckHoverIcon,
+  checkCircleClicked: CheckCircleClickedIcon,
+  checkCircleDefault: CheckCircleDefaultIcon,
+  checkCircleHover: CheckCircleHoverIcon,
+  clockClicked: ClockClickedIcon,
+  clockDefault: ClockDefaultIcon,
+  clockHover: ClockHoverIcon,
+  closeClicked: CloseClickedIcon,
+  closeDefault: CloseDefaultIcon,
+  closeHover: CloseHoverIcon,
+  commentClicked: CommentClickedIcon,
+  commentDefault: CommentDefaultIcon,
+  commentHover: CommentHoverIcon,
+  dashboardClicked: DashboardClickedIcon,
+  dashboardDefault: DashboardDefaultIcon,
+  dashboardHover: DashboardHoverIcon,
+  deleteClicked: DeleteClickedIcon,
+  deleteDefault: DeleteDefaultIcon,
+  deleteHover: DeleteHoverIcon,
+  deletetableDefault: DeletetableDefaultIcon,
+  deletetableVariant2: DeletetableVariant2Icon,
+  docuClicked: DocuClickedIcon,
+  docuDefault: DocuDefaultIcon,
+  docuHover: DocuHoverIcon,
+  downloadClicked: DownloadClickedIcon,
+  downloadDefault: DownloadDefaultIcon,
+  downloadHover: DownloadHoverIcon,
+  editClicked: EditClickedIcon,
+  editDefault: EditDefaultIcon,
+  editHover: EditHoverIcon,
+  edittableDefault: EdittableDefaultIcon,
+  edittableVariant2: EdittableVariant2Icon,
+  eyecloseClicked: EyecloseClickedIcon,
+  eyecloseDefault: EyecloseDefaultIcon,
+  eyecloseHover: EyecloseHoverIcon,
+  eyeopenClicked: EyeopenClickedIcon,
+  eyeopenDefault: EyeopenDefaultIcon,
+  eyeopenHover: EyeopenHoverIcon,
+  faqClicked: FaqClickedIcon,
+  faqDefault: FaqDefaultIcon,
+  faqHover: FaqHoverIcon,
+  feedbackClicked: FeedbackClickedIcon,
+  feedbackDefault: FeedbackDefaultIcon,
+  feedbackHover: FeedbackHoverIcon,
+  filesClicked: FilesClickedIcon,
+  filesDefault: FilesDefaultIcon,
+  filesHover: FilesHoverIcon,
+  gotoClicked: GotoClickedIcon,
+  gotoDefault: GotoDefaultIcon,
+  gotoHover: GotoHoverIcon,
+  homeClicked: HomeClickedIcon,
+  homeDefault: HomeDefaultIcon,
+  homeHover: HomeHoverIcon,
+  logoutClicked: LogoutClickedIcon,
+  logoutDefault: LogoutDefaultIcon,
+  logoutHover: LogoutHoverIcon,
+  menuClicked: MenuClickedIcon,
+  menuDefault: MenuDefaultIcon,
+  menuHover: MenuHoverIcon,
+  messageClicked: MessageClickedIcon,
+  messageDefault: MessageDefaultIcon,
+  messageHover: MessageHoverIcon,
+  moreClicked: MoreClickedIcon,
+  moreDefault: MoreDefaultIcon,
+  moreHover: MoreHoverIcon,
+  nficonsManagement: NficonsManagementIcon,
+  nficonsNegativeAlert: NficonsNegativeAlertIcon,
+  nficonsPanels: NficonsPanelsIcon,
+  nficonsPositiveAlert: NficonsPositiveAlertIcon,
+  nficonsReminder: NficonsReminderIcon,
+  notificationClicked: NotificationClickedIcon,
+  notificationDefault: NotificationDefaultIcon,
+  notificationHover: NotificationHoverIcon,
+  notifyClicked: NotifyClickedIcon,
+  notifyDefault: NotifyDefaultIcon,
+  notifyHover: NotifyHoverIcon,
+  peopleBold: PeopleBoldIcon,
+  peopleLinear: PeopleLinearIcon,
+  peopleTwotone: PeopleTwotoneIcon,
+  profileClicked: ProfileClickedIcon,
+  profileDefault: ProfileDefaultIcon,
+  profileHover: ProfileHoverIcon,
+  proponentsClicked: ProponentsClickedIcon,
+  proponentsDefault: ProponentsDefaultIcon,
+  proponentsHover: ProponentsHoverIcon,
+  repoClicked: RepoClickedIcon,
+  repoDefault: RepoDefaultIcon,
+  repoHover: RepoHoverIcon,
+  searchClicked1: SearchClicked1Icon,
+  searchClicked: SearchClickedIcon,
+  searchDefault: SearchDefaultIcon,
+  settingsClicked: SettingsClickedIcon,
+  settingsDefault: SettingsDefaultIcon,
+  settingsHover: SettingsHoverIcon,
+  shareClicked: ShareClickedIcon,
+  shareDefault: ShareDefaultIcon,
+  shareHover: ShareHoverIcon,
+  signClicked: SignClickedIcon,
+  signDefault: SignDefaultIcon,
+  signHover: SignHoverIcon,
+  sortClicked: SortClickedIcon,
+  sortDefault: SortDefaultIcon,
+  sortHover: SortHoverIcon,
+  tableClicked: TableClickedIcon,
+  tableDefault: TableDefaultIcon,
+  tableHover: TableHoverIcon,
+  uploadClicked: UploadClickedIcon,
+  uploadDefault: UploadDefaultIcon,
+  uploadHover: UploadHoverIcon,
+};
+
+export type IconName = keyof typeof iconRegistry;
