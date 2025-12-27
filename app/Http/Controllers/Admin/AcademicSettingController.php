3425a36e9f4f40f89357903ca8286d28137b6b7d
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2025-12-28 00:52:46 +0800

    feat(admin/acad): utilize date picker and backend data fetching

--- app/Http/Controllers/Admin/AcademicSettingController.php
+++ app/Http/Controllers/Admin/AcademicSettingController.php
@@ -21,12 +21,46 @@ class AcademicSettingController extends Controller
                                 ->first();
 
         // Get the value for active s.y. and sem
-        $active_acad_year = $active?->schoolYear?->year;
-        $active_semestral = $active?->semester;
+        $active_acad_year               = $active?->schoolYear?->year;
+        $active_semestral               = $active?->semester;
+
+        // --- PREPARE THE DATA FOR THE DYNAMIC INPUTED DROPDOWM ---
+        // Get all the school years that in the scoped range
+        $currentYear = (int) date('Y');
+        $valid_sy = SchoolYear::with('semesters')
+                                ->whereBetween('year', [$currentYear - 2, $currentYear + 1])
+                                ->get()
+                                ->keyBy('year');
+
+        // Make the options for dynamic dropdown in academic configuration
+        $schoolYears = [];
+
+        for ($year = $currentYear - 2; $year <= $currentYear; $year++) {
+            $sy = $valid_sy->get($year);
+            $sy_semesters = [];
+
+            if ($sy) {
+                foreach ($sy->semesters as $sem) {
+                    $sy_semesters[$sem->semester] = [
+                        'start' => $sem->start_date?->toDateString(),
+                        'end'   => $sem->end_date?->toDateString(),
+                    ];
+                }
+            }
+
+            $schoolYears[$year] = [
+                'start'     => $sy?->start_date?->toDateString(),
+                'end'       => $sy?->end_date?->toDateString(),
+                'semesters' => $sy_semesters,
+            ];
+        };
+
+        // dd($schoolYears);
 
         return Inertia::render('Admin/management/academic', [
             'active_sy'     => $active_acad_year,
             'active_sem'    => $active_semestral,
+            'school_year'   => $schoolYears
         ]);
     }
 
--- resources/js/pages/Admin/management/academic.tsx
+++ resources/js/pages/Admin/management/academic.tsx
@@ -3,6 +3,9 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import { academic } from '@/routes/admin/management/index';
+import { Label } from '@/components/ui/label';
+import { DatePicker } from '@/components/date-picker';
+import { useState } from 'react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,9 +14,21 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+type SchoolYearItem = {
+    start: string | null;
+    end: string | null;
+    semesters: Record<
+        number, {
+            start: string | null;
+            end: string | null;
+        }
+    >;
+};
+
 type AcademicPageProps = {
     active_sy: number | null;
     active_sem: number | null;
+    school_year: Record<number, SchoolYearItem>;
 };
 
 // HELPER: Create Valid and Scoped Range of possible s.y.
@@ -28,26 +43,31 @@ const mapSemester: Record<number, string> = {
     1: '2nd Semester',
 };
 
-// Dnamic options for academic year
-export function getAcademicYears(): AcademicYear[] {
-    const currentYear: number = new Date().getFullYear();
-    const pastRange = 2;
-    const years: AcademicYear[] = [];
 
-    // Loop from the past offset to the future offset
-    for (let year = currentYear - pastRange; year <= currentYear; year++) {
-        years.push({
-            value: year,
-            label: `${year}–${year + 1}`,
-        });
-    }
+export default function AcademicPage({ active_sy , school_year, active_sem }: AcademicPageProps) {
+    const [acadDate, setAcadDate] = useState<{
+        start: Date | null; end: Date | null;
+    }>({
+        start: active_sy ? new Date(school_year[active_sy].start!) : null,
+        end: active_sy ? new Date(school_year[active_sy].end!) : null,
+    });
 
-    return years;
-}
+    const [selectedSy, setSelectedSy] = useState<number | null>(active_sy);
 
+    // HELPER: change the datepicker value default dependent on the selected year
+    const handleSyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newYearId = Number(e.target.value);
+        setSelectedSy(newYearId); 
 
-export default function AcademicPage({ active_sy, active_sem }: AcademicPageProps) {
-    const academicYears = getAcademicYears();
+        const selectedRecord = school_year[newYearId];
+
+        if (selectedRecord) {
+            setAcadDate({
+                start: selectedRecord.start ? new Date(selectedRecord.start) : null,
+                end: selectedRecord.end ? new Date(selectedRecord.end) : null,
+            });
+        }
+    };
 
     return (
         <ManagementLayout 
@@ -57,38 +77,110 @@ export default function AcademicPage({ active_sy, active_sem }: AcademicPageProp
         >
             <div className="flex flex-1 flex-row gap-4"> 
                 <div className="flex flex-1 flex-col gap-4">
-                    <div className="flex-1 p-4 py-6 rounded-xl border border-sidebar-border/70">
+                    <div className="flex flex-1 flex-col p-4 py-6 rounded-xl border border-sidebar-border/70 gap-5">
                         {/* Academic year settings */}
-                        <label className="mb-2 block text-sm font-medium text-muted-foreground">
-                            Set the Active Academic Year:
-                        </label>
-                        <select defaultValue={active_sy ? active_sy : ""} className="w-full rounded-md border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ring">
-                            <option value="" disabled>Select an option</option>
-                            {academicYears.map((year) => (
-                                <option key={year.value} value={year.value}>
-                                    {year.label}
-                                </option>
-                            ))}
-                        </select>
-                        <p className='pt-5 font-bold text-md text-center text-primary'>
-                            Active School Year: { active_sy ? `${active_sy}–${Number(active_sy) + 1}` : 'Not yet Activated!'}
+                        <div>
+                            <label className="mb-2 block text-sm font-medium text-muted-foreground">
+                                Set the Active Academic Year:
+                            </label>
+                            <select 
+                                className="w-full rounded-md border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ring"
+                                value={selectedSy ?? ""}
+                                onChange={handleSyChange}
+                            >
+                                <option value="" disabled>Select an option</option>
+                                {Object.keys(school_year).map((yr) => {
+                                    const year = Number(yr);
+
+                                    // dynamic sychool year options from the backend
+                                    return (
+                                        <option key={year} value={year}>
+                                            {year} – {year + 1}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                        
+                        <div className='flex flex-row gap-5 flex-1'>
+                            <div className='flex-1'>
+                                <Label className="text-primary">Start Date</Label>
+                                <DatePicker
+                                    key={`sy-start-${selectedSy}`}  // id for changing defaults
+                                    displayFormat="full"
+                                    value={acadDate.start ?? undefined}
+                                    placeholder="Select Start Date"
+                                    onChange={(date) => {
+                                        setAcadDate((prev) => ({ ...prev, start: date })); 
+                                        console.log("Updated Acad Date:", acadDate);
+                                    }}
+                                />
+                            </div>
+                            <div className='flex-1'>
+                                <Label className="text-primary">End Date</Label>
+                                <DatePicker
+                                    key={`sy-end-${selectedSy}`}    // id for changing defaults
+                                    displayFormat="full"
+                                    value={acadDate.end ?? undefined}
+                                    placeholder="Select End Date"
+                                    onChange={(date) => {
+                                        setAcadDate((prev) => ({ ...prev, end: date })); 
+                                        console.log("Updated Acad Date:", acadDate);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        
+                        <p className='pt-5 font-bold text-md text-center text-primary flex-1'>
+                            Current Active School Year: { active_sy ? `${active_sy}–${Number(active_sy) + 1}` : 'Not yet Activated!'}
                         </p>
                     </div>
                     
-                    <div className="flex-1 p-4 py-6 rounded-xl border border-sidebar-border/70">
+                    <div className="flex flex-1 flex-col p-4 py-6 rounded-xl border border-sidebar-border/70 gap-5">
                         {/* Semestral settings */}
-                        <label className="mb-2 block text-sm font-medium text-muted-foreground">
-                            Set the Active Semestral:
-                        </label>
-                        <select 
-                            className="w-full rounded-md border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ring"
-                            defaultValue={active_sem ? active_sem + 1 : ""} 
-                        >
-                            <option value="" disabled>Select an option</option>
-                            <option value="1">1st Semester</option>
-                            <option value="2">2nd Semester</option>
-                        </select>
-                        <p className='pt-5 font-bold text-md text-center text-primary'>
+                        <div>
+                            <label className="mb-2 block text-sm font-medium text-muted-foreground">
+                                Set the Active Semestral:
+                            </label>
+                            <select 
+                                className="w-full rounded-md border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ring"
+                                defaultValue={active_sem ? active_sem + 1 : ""} 
+                            >
+                                <option value="" disabled>Select an option</option>
+                                <option value="1">1st Semester</option>
+                                <option value="2">2nd Semester</option>
+                            </select>
+                        </div>
+
+
+                        <div className='flex flex-row gap-5 flex-1'>
+                            <div className='flex-1'>
+                                <Label className="text-primary">Start Date</Label>
+                                <DatePicker
+                                    displayFormat="full"
+                                    value={acadDate.start ?? undefined}
+                                    placeholder="Select Start Date"
+                                    onChange={(date) => {
+                                        setAcadDate((prev) => ({ ...prev, start: date })); 
+                                        console.log("Updated Acad Date:", acadDate);
+                                    }}
+                                />
+                            </div>
+                            <div className='flex-1'>
+                                <Label className="text-primary">End Date</Label>
+                                <DatePicker
+                                    displayFormat="full"
+                                    value={acadDate.end ?? undefined}
+                                    placeholder="Select End Date"
+                                    onChange={(date) => {
+                                        setAcadDate((prev) => ({ ...prev, end: date })); 
+                                        console.log("Updated Acad Date:", acadDate);
+                                    }}
+                                />
+                            </div>
+                        </div>
+
+                        <p className='pt-5 font-bold text-md text-center text-primary flex-1'>
                             Active Semester: {active_sem ? mapSemester[active_sem] ?? `Semester ${active_sem}` : 'Not yet Activated!'}
                         </p>
                     </div>
