0af44a2606c16f77e86f93f1dbc9ebf1552b1303
amplaguador <angelinelaguador0@gmail.com>
2026-01-03 18:59:14 +0800

    _

--- resources/js/pages/Admin/management/student.tsx
+++ resources/js/pages/Admin/management/student.tsx
@@ -164,7 +164,6 @@ export default function StudentManagement({ students }: { students?: any[] }) {
           }}
         />
       </ManagementLayout>
-          
       <NavFooter />
     </>
   );
