09406e75742be60e865520c45fac5277ad6b99eb
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2026-01-06 22:21:54 +0800

    fix(layout): change flew row to column as default

--- resources/js/components/app-content.tsx
+++ resources/js/components/app-content.tsx
@@ -73,7 +73,7 @@ export function AppContent({
           </div>
         </div>
       )}
-      <div className="flex flex-1 mx-auto w-full max-w-[1440px] p-6 h-full">
+      <div className="flex flex-1 flex-col mx-auto w-full max-w-[1440px] p-6 h-full">
         {children}
       </div>
     </div>
