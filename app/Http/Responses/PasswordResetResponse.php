c660bb1e7e31ed9671b813a163a46f60f9cd995a
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2026-07-25 00:32:24 +0800

    fix(fortify): bind custom PasswordResetResponse to avoid hardcoded route login crash

---
+++ app/Http/Responses/PasswordResetResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\JsonResponse;
+use Laravel\Fortify\Contracts\PasswordResetResponse as PasswordResetResponseContract;
+
+class PasswordResetResponse implements PasswordResetResponseContract
+{
+    public function __construct(protected string $status) {}
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        return $request->wantsJson()
+            ? new JsonResponse(['message' => trans($this->status)], 200)
+            : redirect()->route('login')->with('status', trans($this->status));
+    }
+}
